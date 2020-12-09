41343938689876633d38036fc7f40033cd58ec27
Joey <joeydocter@gmail.com>
2020-12-09 18:57:22 +0100

    Grenade
    player can shoot his grenade

---
+++ GDV_Docter_Joey_1/Assets/Scripts/Weapon Script/Grenades.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenades : MonoBehaviour
+{
+    private float delay = 3f;
+    private float radius = 5f;
+    private float force = 700f;
+
+    public GameObject explosionEffect;
+
+    private float countdown;
+
+    private bool hasExploded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+           Instantiate(explosionEffect, transform.position, transform.rotation);
+           Collider[] collidersToHit = Physics.OverlapSphere(transform.position, radius);
+
+              foreach(Collider nearbyObject in collidersToHit)
+              {
+                  TargetScript target = nearbyObject.GetComponent<TargetScript>();
+
+                  if(target != null)
+                  {
+                      target.TakeDamage(40);
+                  }
+              }
+
+              Collider[] collidersToMove = Physics.OverlapSphere(transform.position, radius);
+
+              foreach (Collider nearbyObject in collidersToMove)
+              {
+                  Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+                  if (rb != null)
+                  {
+                      rb.AddExplosionForce(force, transform.position, radius);
+                  }
+              }
+
+              Destroy(gameObject);
+    }
+}
---
+++ GDV_Docter_Joey_1/Assets/Scripts/Weapon Script/ThrowGrenade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGrenade : MonoBehaviour
+{
+    private float throwForce = 20f;
+    public GameObject grenadePrefab;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Throw();
+        }
+    }
+
+    void Throw()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+}
